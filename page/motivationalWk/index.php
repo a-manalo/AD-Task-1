02ab9066300310f201a4417e13b66524728add01
a-manalo <agmnlo.by@gmail.com>
2025-05-11 03:48:24 +0800

    feat(weekpage): copied navbar and declared array of motivational quotes for each day of the week

--- page/motivationalWk/index.php
+++ page/motivationalWk/index.php
@@ -4,8 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Weekly Motivational Quotes</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 <body>
-    
+    <nav id="navbar">
+        <div id="nav-brand"> Motivational Quotes </div>
+        <ul id="nav-links">
+            <li><a href="/index.php"> Home </a></li>
+            <li><a href=".page/motivationalWk/index.php"> Weekly Quotes </a></li>
+        </ul>
+    </nav>
+
+    <?php
+        $days = [
+            'Monday' => "The only way to do great work is to love what you do.",
+            'Tuesday' => "Success is not final, failure is not fatal. It is the courage to continue that counts.",
+            'Wednesday' => "Believe you can and you're halfway there.",
+            'Thursday' => "Don't watch the clock, do what it does. Keep going.",
+            'Friday' => "The future belongs to those who believe in the beauty of their dreams.",
+            'Saturday' => "Everything you've ever wanted is on the other side of fear.",
+            'Sunday' => "The best way to predict the future is to create it."
+        ];
+    ?>
 </body>
 </html>
