72ba096e4726dfe6a47fd6af64f6ce60feaae02a
a-manalo <agmnlo.by@gmail.com>
2025-05-11 16:06:08 +0800

    style(weekpage): formatted the quotes

--- page/motivationalWk/index.php
+++ page/motivationalWk/index.php
@@ -20,9 +20,9 @@
     <?php
         $days = [
             'Monday' => "The only way to do great work is to love what you do.",
-            'Tuesday' => "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+            'Tuesday' => "Success is not final, failure is not fatal. It is the courage to continue that counts.",
             'Wednesday' => "Believe you can and you're halfway there.",
-            'Thursday' => "Don't watch the clock; do what it does. Keep going.",
+            'Thursday' => "Don't watch the clock, do what it does. Keep going.",
             'Friday' => "The future belongs to those who believe in the beauty of their dreams.",
             'Saturday' => "Everything you've ever wanted is on the other side of fear.",
             'Sunday' => "The best way to predict the future is to create it."
