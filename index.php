d7bbf478c5987a7cb7b0a664910600089a03be8e
a-manalo <agmnlo.by@gmail.com>
2025-05-11 15:59:48 +0800

    style(homepage): added id="active" to home link for styling and some comments

--- index.php
+++ index.php
@@ -7,14 +7,16 @@
     <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 <body>
+    <!-- navigation bar for site branding and page links -->
     <nav id="navbar">
         <div id="nav-brand"> Motivational Quotes </div>
         <ul id="nav-links">
-            <li><a href="/index.php"> Home </a></li>
+            <li><a href="/index.php" id="active"> Home </a></li>
             <li><a href="./page/motivationalWk/index.php"> Weekly Quotes </a></li>
         </ul>
     </nav>
 
+    <!-- container with welcome msg, description, and button to weekly quotes -->
     <div id="container">
         <h1 id="title"> Welcome to Motivational Quotes </h1>
         <p id="description"><?php echo "Discover daily inspiration with our motivational quotes for the week. Each day brings you a new perspective to help you stay motivated" ?></p>
